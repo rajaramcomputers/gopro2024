a82ded3075a14d805027820d606ae7778a6543ad
rajaramcomputers <rajaramcomputers@gmail.com>
2024-01-01 23:18:04 +0400

    added the day1 code - usage of array

---
+++ day1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [3]int = [3]int{1, 2, 3}
+	//print the indices and values
+	for i, v := range a {
+		fmt.Printf("The value %d is at %d\n", v, i)
+	}
+	fmt.Printf("The value in the array are: ")
+	for _, v := range a {
+		fmt.Printf("%d , ", v)
+	}
+	// usage of ...
+	type Currency int
+
+	const (
+		USD Currency = iota
+		EUR
+		GBP
+		RMB
+	)
+	symbol := [...]string{USD: "$", EUR: "€", GBP: "£", RMB: "¥"}
+	fmt.Println(RMB, symbol[RMB])
+}
